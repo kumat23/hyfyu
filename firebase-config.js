a0efd0b0283e00da8fcf74a1fd2f4a5a2c96ccd4
kumat23 <155309178+kumat23@users.noreply.github.com>
2023-12-31 19:38:51 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQxgMR6egIJ3u6AJFppP3Q554cWlLSbhU",
+  authDomain: "guayg-e6d32.firebaseapp.com",
+  databaseURL: "https://guayg-e6d32-default-rtdb.firebaseio.com",
+  projectId: "guayg-e6d32",
+  storageBucket: "guayg-e6d32.appspot.com",
+  messagingSenderId: "707397641400",
+  appId: "1:707397641400:web:bf9deddcafaeb1f177004d"
+};
